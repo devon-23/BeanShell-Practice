ebc6922fc0d78a0c55a596006aa48d27356f7dd3
Devonnn <63268642+devon-23@users.noreply.github.com>
2024-07-16 14:00:27 -0400

    file reader
    Wrote to a file, then read from it using BeanShell

--- simple/src/simple/Main.java
+++ simple/src/simple/Main.java
@@ -9,7 +9,21 @@ public class Main {
 			Interpreter interpreter = new Interpreter();
 			FileReader fileReader = new FileReader("src/script.bsh");
 			
+			Interpreter bsh = new Interpreter();
+			
 			Object result = interpreter.eval(fileReader);
+			
+			bsh.set("x", 15);
+			bsh.set("y", 20);
+			int sum = (int) bsh.eval("x + y");
+			
+			System.out.println("The sum of " + bsh.get("x") + " + " + bsh.get("y") + " is " + sum);
+			
+			String script = "int multiply(int a, int b) { return a * b; }";
+			bsh.eval(script);
+
+			int product = (Integer) bsh.eval("multiply(3, 4);");
+			System.out.println(product);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
