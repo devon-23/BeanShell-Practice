ba9adb36fad4b908fdb86e7e418a0c4a40ef3850
Devonnn <63268642+devon-23@users.noreply.github.com>
2024-08-01 13:29:38 -0400

    String Buffer

--- anotherProject/src/anotherProject/Main.java
+++ anotherProject/src/anotherProject/Main.java
@@ -1,6 +1,7 @@
 package anotherProject;
 
 import bsh.Interpreter;
+import java.lang.StringBuffer;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,5 +15,28 @@ public class Main {
 			e.printStackTrace();
 		}
 		
+		StringBuffer sb = new StringBuffer("Hello");
+		sb.append(" World");
+		System.out.println("After append: " + sb);
+		
+		//Insert text at specific part
+		sb.insert(6, "Beautiful ");
+		System.out.println("After insert: " + sb);
+		
+		// Replace text from index 6 to 13
+		sb.replace(6,  15, "Amazing");
+		System.out.println("After replace: " + sb);
+		
+		// Delete text from index 6 to 13
+		sb.delete(6,  14);
+		System.out.println("After delete: " + sb);
+		
+		// Reverse the string
+		sb.reverse();
+		System.out.println("After reverse: " + sb);
+		
+		// Convert to string
+		String finalStr = sb.toString();
+		System.out.println("Final String: " + sb);
 	}
 }
